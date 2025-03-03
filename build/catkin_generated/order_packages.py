b365741dd8052f9ab906161921d1deb81777c1aa
mike522 <64064561+mike522-byte@users.noreply.github.com>
2025-03-03 20:10:12 +0800

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/pi/autonomous_driving_Class-master/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/pi/ROS_WS/devel;/opt/ros/melodic'.split(';') if '/home/pi/ROS_WS/devel;/opt/ros/melodic' != '' else []
